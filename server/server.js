1de939a63a53ee2d7ff386037c4463a5c2bbb32e
Papay Balint <146977619+papaybalint@users.noreply.github.com.>
2026-08-27 16:15:35 +0200

    Implement dotenv configuration, CORS, and JSON parsing for server environment variables

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hellooo Gintli! Működik ez a szar Hot Reload! xd')
-
+  res.send('CineMatch API is running!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
